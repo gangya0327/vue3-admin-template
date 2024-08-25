d7b568a0ad87578823d09a2d45615ed6c09a12b0
gangya0327 <king9999427@163.com>
2024-08-26 00:44:03 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <p>{{ str }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = 'abc'
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 300px;
+  height: 200px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  p {
+    color: blueviolet;
+    text-decoration: underline;
+  }
+}
+</style>
